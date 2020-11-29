4b30c62f182a3ae005c4cf92e9343afbf72f5609
awjlogan <awjlogan@gmail.com>
2020-11-29 11:01:57 +0000

    Move common functions out

--- examples/common/samd-dev.c
+++ examples/common/samd-dev.c
@@ -1,8 +1,15 @@
 #include "samd11.h"
-
+#include "samd-dev.h"
 
 //-----------------------------------------------------------------------------
-static void uart_putc(char c)
+inline void sys_init(void)
+{
+    /* Disable prescalar -> 8 MHz core clock */
+    SYSCTRL->OSC8M.bit.PRESC = 0;
+}
+
+//-----------------------------------------------------------------------------
+void uart_putc(const char c)
 {
     while (!(SERCOM0->USART.INTFLAG.reg & SERCOM_USART_INTFLAG_DRE)) {
         SERCOM0->USART.DATA.reg = c;
--- examples/common/samd-dev.h
+++ examples/common/samd-dev.h
@@ -2,4 +2,11 @@
 
 /* Collection of common routines for SAMD1x development board */
 
+#ifndef _SAMD_DEV_H_
+#define _SAMD_DEV_H_
+
+void sys_init(void);
 void uart_puts(const char *s);
+void uart_putc(const char c);
+
+#endif
